756c0bb5997acce7f38f660f10dfe36981ca5df5
Ioannis-Raptis <ioannis_raptis@hotmail.com>
2020-07-27 20:09:32 +0300

    Fix some bugs, break down to multiple files

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-import pickle
-import re
-
-import nltk
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+from predict import make_prediction
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0f1ee8b5e176fe7108309291fc363a04'
-model = pickle.load(open('model.pkl', 'rb'))
-cv = pickle.load(open('cv.pkl', 'rb'))
 
 
 class TweetForm(FlaskForm):
@@ -23,45 +17,18 @@ class TweetForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = TweetForm()
-    if form.validate_on_submit():
-        return redirect(url_for('predict'))
+    if request.method == 'POST':
+        return redirect(url_for('predict', tweet=request.form['tweet']))
     return render_template('home.html', form=form)
-
-
-def process_review(new_review):
-    new_review = str(new_review)
-    new_review = re.sub(r'@\w+', '', new_review)
-    new_review = re.sub('[^a-zA-Z]', ' ', new_review)
-    new_review = new_review.lower()
-    new_review = new_review.split()
-    ps = PorterStemmer()
-    all_stopwords = stopwords.words('english')
-    all_stopwords.remove('not')
-    new_review = [ps.stem(word)
-                  for word in new_review if not word in set(all_stopwords)]
-    new_review = ' '.join(new_review)
-    new_corpus = [new_review]
-    return new_corpus
-
-
-def predict_sentiment(new_corpus):
-    new_X_test = cv.transform(new_corpus).toarray()
-    new_y_pred = model.predict(new_X_test)
-    return new_y_pred
-
-
-sentiment_dict = {
-    '[0]': 'negative',
-    '[1]': 'neutral',
-    '[2]': 'positive'
-}
 
 
 @app.route('/predict')
 def predict():
-    processed_tweet = process_review(request.form.values())
-    prediction = predict_sentiment(processed_tweet)
-    return render_template('predict.html', prediction_text=f'The tweet has a {sentiment_dict[str(prediction)]} sentiment')
+    tweet = request.args.get('tweet', None)
+    if tweet == None:
+        return redirect(url_for('home'))
+    prediction = make_prediction(tweet)
+    return render_template('predict.html', prediction_text=prediction)
 
 
 if __name__ == "__main__":
---
+++ predict.py
@@ -0,0 +1,44 @@
+import pickle
+import re
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+model = pickle.load(open('model.pkl', 'rb'))
+cv = pickle.load(open('cv.pkl', 'rb'))
+
+
+def preprocess(new_review):
+    new_review = re.sub(r'@\w+', '', new_review)
+    new_review = re.sub('[^a-zA-Z]', ' ', new_review)
+    new_review = new_review.lower()
+    new_review = new_review.split()
+    ps = PorterStemmer()
+    all_stopwords = stopwords.words('english')
+    all_stopwords.remove('not')
+    new_review = [ps.stem(word)
+                  for word in new_review if not word in set(all_stopwords)]
+    new_review = ' '.join(new_review)
+    new_corpus = [new_review]
+    return new_corpus
+
+
+def predict_sentiment(new_corpus):
+    new_X_test = cv.transform(new_corpus).toarray()
+    new_y_pred = model.predict(new_X_test)
+    return new_y_pred
+
+
+sentiment_dict = {
+    '[0]': 'negative',
+    '[1]': 'neutral',
+    '[2]': 'positive'
+}
+
+
+def make_prediction(s):
+    print(str(s))
+    s = preprocess(str(s))
+    prediction = predict_sentiment(s)
+    return f'The tweet has a {sentiment_dict[str(prediction)]} sentiment'
